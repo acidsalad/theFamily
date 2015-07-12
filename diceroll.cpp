0c50cde87cf4b5ee683dbcfa072b04cdbe0d70a5
acidsalad <brenda@bastardi.net>
2015-07-12 09:38:16 -0500

    update diceroll.cpp with an attack feature

--- diceroll.cpp
+++ diceroll.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-	default_random_engine rando;
+	static default_random_engine rando(time(NULL));
 	uniform_int_distribution<int> diceRoll(1, 6);
 	
 	cout << "You rolled a " << diceRoll(rando) << endl;
+	
+	uniform_real_distribution<float> attackRoll(0.0f, 1.0f);
+	
+	float attack = attackRoll(rando);
+	cout << "You attack your unloving father!" << endl;
+	if (attack <= 0.7f)
+	{
+		cin.get();
+		cout << "No damage." << endl;
+		cout << "Your father said you hit like a girl." << endl;
+	}
+	else 
+	{
+		cin.get();
+		cout << "A direct hit!" << endl;
+		cout << "A sparkling tear forms in your father's unloving eyes." << endl;
+	}
 	
 	int temp;
 	cin >> temp;
